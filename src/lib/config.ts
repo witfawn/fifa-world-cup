cd08ad73f76b28411d3e2c29595df1d7dde7130a
John Pontefract <john@witfawn.com>
2026-06-05 12:00:04 -0700

    fix: entry fee is $100 not $10

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
 // App configuration
 export const VENMO_LINK = "https://account.venmo.com/pay?amount=100.00&note=WC%20organizer&recipients=John-Pontefract&txn=pay&audience=private";
-export const ENTRY_FEE = 10; // dollars
+export const ENTRY_FEE = 100; // dollars
 export const ADMIN_EMAILS = ["john@witfawn.com"];
